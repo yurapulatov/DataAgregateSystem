744cabb362c4fd479547443c6aa0dca58b69dae8
Пулатов Юрий Ганиджанович <yurapulatov@mail.ru>
2019-04-22 19:09:48 +0700

    Entities + DbContext

---
+++ DataAgregateSystem/Common/Entities/FacebookData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Common.Entities
+{
+    public class FacebookData
+    {
+        public long Id { get; set; }
+        public DateTime? DateParse { get; set; }
+        public long Value { get; set; }
+        public long FacebookPageId { get; set; }
+        
+    }
+}
---
+++ DataAgregateSystem/Infrastructure/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Common.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<FacebookData> FacebookData { get; set; }
+        public DbSet<FacebookPage> FacebookPages { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=usersdb;Username=postgres;Password=password");
+        }
+    }
+}
